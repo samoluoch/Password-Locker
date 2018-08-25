3a1c3e5d1383b52252e177523dce48b35b517c41
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-25 16:54:16 +0300

    added the tested functions into the main script run.py

--- run.py
+++ run.py
@@ -5,5 +5,25 @@ from credential_data import Credential
 def create_user(user_name, phone_number, email):
     new_user = User(user_name, phone_number, email)
     return new_user
+    
+
+def save_user(user):
+    user.save_user()
+    
+
+def delete_user(user):
+    user.delete_user()
 
 
+def find_by_username(username):
+    return User.find_by_username(username)
+
+
+def user_exists(username):
+    return User.user_exist(username)
+
+
+def display_user():
+    return User.display_user()
+
+def main():
