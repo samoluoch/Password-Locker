116c364eb356db7f6e4ad06be9350f1ad06edeca
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-26 17:11:25 +0300

    created a function for the short code cu for the user to select when they want to create a new user in the run.py

--- run.py
+++ run.py
@@ -27,3 +27,33 @@ def display_user():
     return User.display_user()
 
 def main():
+    print("Welcome to Password Locker. Please enter your username to proceed")
+    user_name = input()
+
+    print(f"Thank you {user_name}. Use the following options to proceed:")
+    print('\n')
+
+    while True:
+        print("Enter code cu to create a new user, du to display users, fc to find users, and ex to exit")
+
+        code = input().lower()
+
+        if code == 'cu':
+            print("New user")
+            print("-"*10)
+
+            print("Username...")
+            user_name = input()
+
+            print("Phone number...")
+            phone_number = input()
+
+            print("User email...")
+            email = input()
+
+            save_user(create_user(user_name, phone_number, email)
+            print('\n')
+            print(f"New User {user_name} {phone_number} {email} created)
+            print('\n')
+
+        elif code == 'du':
